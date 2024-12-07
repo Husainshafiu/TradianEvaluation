62202c4ddda8b478124fa78b6972c1003343241d
Shafiu <Husain.shafiu@gmail.com>
2024-12-07 19:31:30 +0500

    added in db

---
+++ Nextjs/tradian/lib/db/index.ts
@@ -0,0 +1,7 @@
+import { drizzle } from "drizzle-orm/neon-serverless";
+import * as schema from "./schema";
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+}
+export const db = drizzle(process.env.DATABASE_URL, { schema });
