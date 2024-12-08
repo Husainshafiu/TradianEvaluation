f213fa424342caf1d373e5455a32e8a3c4a90789
Shafiu <Husain.shafiu@gmail.com>
2024-12-08 17:47:40 +0500

    updates

--- DotNetCore/TradianEvaluation.web/Controllers/HomeController.cs
+++ DotNetCore/TradianEvaluation.web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Support()
+    {
+        return View();
+    }
+
 
     [HttpGet]
     public IActionResult GetDeclarationForm()
